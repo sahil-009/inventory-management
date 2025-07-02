9d2202d8b01ccfb6b2a82b47eadad3bf4b1d8f1b
sahil-009 <sahilmund.6685@gmail.com>
2025-07-02 19:48:40 +0530

    added fs

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div>
+      <h1>dashboardWrapper</h1>
+      {children}
+    </div>
+  )
+}
+
+export default DashboardWrapper
